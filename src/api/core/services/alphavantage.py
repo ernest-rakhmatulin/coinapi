0a31c1d0beee738700149744b470595dcab70014
Ernest Rakhmatulin <ernest.rakhmatulin@gmail.com>
2022-04-20 23:39:52 +0300

    Added refresh_currency_exchange_rate to alphavantage.py.

--- src/api/core/services/alphavantage.py
+++ src/api/core/services/alphavantage.py
@@ -1,21 +1,35 @@
 import requests
 from django.conf import settings
 
-DEFAULT_FUNCTION = 'CURRENCY_EXCHANGE_RATE'
-DEFAULT_PARAMS = {'from_currency': 'BTC', 'to_currency': 'USD'}
+from core.models import CurrencyExchangeRate
 
 
 def query_alphavantage(function: str, params: dict):
+    url = 'https://www.alphavantage.co/query'
     query_params = {
         'function': function,
         'apikey': settings.ALPHA_VANTAGE_API_KEY,
         **params
     }
-
-    url = 'https://www.alphavantage.co/query'
     response = requests.get(url, params=query_params)
     return response.json()
 
 
-x = query_alphavantage(function=DEFAULT_FUNCTION, params=DEFAULT_PARAMS)
-print(x)
+def refresh_currency_exchange_rate(from_currency, to_currency):
+    function = 'CURRENCY_EXCHANGE_RATE'
+    params = {'from_currency': from_currency, 'to_currency': to_currency}
+
+    query_response = query_alphavantage(function=function, params=params)
+    rate = query_response['Realtime Currency Exchange Rate']
+
+    exchange_rate = CurrencyExchangeRate(
+        from_code=rate.get("1. From_Currency Code"),
+        to_code=rate.get("3. To_Currency Code"),
+        exchange_rate=rate.get("5. Exchange Rate"),
+        bid_price=rate.get("8. Bid Price"),
+        ask_price=rate.get("9. Ask Price")
+    )
+    exchange_rate.save()
+
+    return exchange_rate
+
