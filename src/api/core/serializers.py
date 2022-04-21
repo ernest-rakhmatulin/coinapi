9c36406e5eaa3f38187563c1a0314041ab85a8ae
Ernest Rakhmatulin <ernest.rakhmatulin@gmail.com>
2022-04-21 18:36:12 +0300

    Added Celery Beat, downgraded Django version to 3.2.13

--- src/api/core/serializers.py
+++ src/api/core/serializers.py
@@ -6,4 +6,7 @@ from core.models import CurrencyExchangeRate
 class CurrencyExchangeRateSerializer(serializers.ModelSerializer):
     class Meta:
         model = CurrencyExchangeRate
-        fields = ['from_code', 'to_code', 'exchange_rate', 'bid_price', 'ask_price', 'refresh_time']
+        fields = [
+            'from_code', 'to_code', 'exchange_rate',
+            'bid_price', 'ask_price', 'refresh_time'
+        ]
